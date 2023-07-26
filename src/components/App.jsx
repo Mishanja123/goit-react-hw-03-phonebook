05b20cf80197165ff851a01d088b5af82ed10447
Misha <shevchenkom606@gmail.com>
2023-07-26 19:41:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,15 @@ export class App extends Component  {
     contacts: [],
     filter: '',
   }
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+  componentDidUpdate() {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
 
   addContact = (contactName, contactNumber, contactId) => {
     if (this.checkName(contactName)) {
